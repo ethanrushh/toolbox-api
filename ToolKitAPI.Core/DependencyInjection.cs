4e9de3ef672e096675f2c48e881f3659d22b2fa2
Ethan Rushbrook <ethanrushbrook@outlook.com>
2023-01-21 23:46:22 +1100

    fix: add core project to git

---
+++ ToolKitAPI.Core/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ToolKitAPI.Core;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddCoreServices(this IServiceCollection serviceCollection)
+    {
+        var localAssembly = typeof(DependencyInjection).Assembly;
+
+        serviceCollection.AddMediatR(localAssembly);
+        
+        
+        return serviceCollection;
+    }
+}

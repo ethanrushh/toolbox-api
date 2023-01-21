24d083bc58b9f664a296fd67698069ec5d2255d8
Ethan Rushbrook <ethanrushbrook@outlook.com>
2023-01-22 00:27:55 +1100

    chore: frameworking for create note handler

---
+++ ToolKitAPI.Core/Commands/Notes/CreateNoteCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using ToolKitAPI.Data.DTOs.Notes;
+
+namespace ToolKitAPI.Core.Commands.Notes;
+
+public record CreateNoteCommand(string Creator, string Content) : IRequest<NoteReadDto>;
---
+++ ToolKitAPI.Core/Handlers/CreateNoteHandler.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using ToolKitAPI.Core.Commands.Notes;
+using ToolKitAPI.Data.DTOs.Notes;
+
+namespace ToolKitAPI.Core.Handlers;
+
+public class CreateNoteHandler : IRequestHandler<CreateNoteCommand, NoteReadDto>
+{
+    public Task<NoteReadDto> Handle(CreateNoteCommand request, CancellationToken cancellationToken)
+    {
+        
+    }
+}

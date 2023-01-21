3922816c58b86202d9f9aac95a4d15ba7ee3288b
Ethan Rushbrook <ethanrushbrook@outlook.com>
2023-01-22 01:40:52 +1100

    feat: validation for delete note command

---
+++ ToolKitAPI.Core/Validators/DeleteNoteCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using ToolKitAPI.Core.Commands.Notes;
+
+namespace ToolKitAPI.Core.Validators;
+
+public class DeleteNoteCommandValidator : AbstractValidator<DeleteNoteCommand>
+{
+    public DeleteNoteCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty()
+            .NotNull()
+            .NotEqual(Guid.Empty);
+    }
+}

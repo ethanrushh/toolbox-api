a8725b05fbf7f4e7e07c145e6d0846f9e02db40a
Ethan Rushbrook <ethanrushbrook@outlook.com>
2023-01-22 01:46:44 +1100

    feat: validation for get notes query

---
+++ ToolKitAPI.Core/Validators/GetNotesQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using ToolKitAPI.Core.Queries;
+
+namespace ToolKitAPI.Core.Validators;
+
+public class GetNotesQueryValidator : AbstractValidator<GetNotesQuery>
+{
+    public GetNotesQueryValidator()
+    {
+        RuleFor(x => x.Quantity)
+            .GreaterThan(0)
+            .When(x => x.Quantity is not null);
+    }
+}
